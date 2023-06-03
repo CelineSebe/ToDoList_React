2f8c8a2fb6c1b731045a8c3ab965c500f91f7c1e
Céline Sèbe <celine.sebe@gmail.com>
2023-06-03 23:52:27 +0200

    handleCheckcircle & handleInput new task

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import TaskCreation from './TaskCreation';
 import Task from './Task';
 import './App.css';
-import { faTrash } from '@fortawesome/free-regular-svg-icons';
+import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const handleTaskCreate = (taskName) => {
+    const newTask = {
+      id: Date.now(),
+      name: taskName
+    };
+    setTasks([...tasks, newTask]);
+  }
+  const handleTaskUpdate = (taskId, newTaskName) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, name: newTaskName };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
   return (
     <div>
-    <div class='todoapp'>
-      <TaskCreation />
-      <Task />
-      
+    <div className='todoapp'>
+      <TaskCreation onTaskCreate={handleTaskCreate}/>
+      {tasks.map(task => (
+      <Task key={task.id} 
+            name={task.name} 
+            initialText={task.name}
+            onUpdate={(newTaskName) => handleTaskUpdate(task.id, newTaskName)} />
+      ))}
     </div>
     </div>
   );
--- src/TaskCreation.js
+++ src/TaskCreation.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 
-function TaskCreation(){
+function TaskCreation(props){
+    const [taskName, setTaskName]= useState('');
+
+    const handleInputChange = (event) => {
+        setTaskName(event.target.value);
+    }
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        props.onTaskCreate(taskName);
+        setTaskName('');
+    }
     return(
 
-            <form>
-                <button> + </button>
-                <input type="text" class='taskcreation' placeholder="Nouvelle tâche"/>
+            <form onSubmit={handleFormSubmit}>
+                <button type="submit"> + </button>
+                <input type="text" 
+                    value={taskName}
+                    onChange={handleInputChange}
+                    className='taskcreation' 
+                    placeholder="Nouvelle tâche"/>
             </form>
         )
 }
