b42ee5ca2db08d7c3ba16530f80a35c94f64402a
Céline Sèbe <celine.sebe@gmail.com>
2023-06-03 18:11:43 +0200

    html structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import TaskCreation from './TaskCreation';
+import Task from './Task';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TaskCreation />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
     </div>
   );
 }
---
+++ src/TaskCreation.js
@@ -0,0 +1,13 @@
+
+function TaskCreation(){
+    return(
+
+        <div className='TaskCreation'>
+            <form>
+                <button> + </button>
+                <input type="text"/>
+            </form>
+        </div>
+        )
+}
+export default TaskCreation;
